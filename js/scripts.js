3207eca2dbf078d113405ec03b47095681e35ec2
Oloo Ouma <oloosolomon@gmail.com>
2019-05-12 14:38:43 +0300

    Add toggle effect on the rules

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,6 @@
 
 $(document).ready(function(){
-    $("#click").click(function(){
-        $("#row").slideToggle();
-    });
-
-    $("#ru").click(function(){
-        $("#rules").slideToggle();
+    $("#chike").click(function(){
+        $("#inst").slideToggle();
     });
 });
