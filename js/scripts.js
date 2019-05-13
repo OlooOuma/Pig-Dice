6e5264bb8cc543cebf19ffeab684ec721515661a
Oloo Ouma <oloosolomon@gmail.com>
2019-05-13 04:30:01 +0300

    Add Math random

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ $(document).ready(function(){
         $("#inst").slideToggle();
     });
 });
+function Player(turn) {
+  this.turn = turn;
+  this.roll = 0;
+  this.turnScore = 0;
+  this.totalScore = 0;
+}
+function rollDice(){
+  return Math.floor(6 * Math.random())+1;
+}
