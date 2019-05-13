7c2a96974080cc4794a5ecd297f493e7835b0130
Oloo Ouma <oloosolomon@gmail.com>
2019-05-13 04:42:36 +0300

    Add js prototype

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,59 @@ function Player(turn) {
 function rollDice(){
   return Math.floor(6 * Math.random())+1;
 }
+Player.prototype.startGame = function() {
+  this.roll = 0;
+  this.turnScore = 0;
+  this.totalScore = 0;
+  this.playerName = "";
+}
+Player.prototype.roll1 = function() {
+  if (this.roll === 1) {
+    this.turnScore = 0;
+    alert("Pass the mouse");
+  } else {
+    this.turnScore += this.roll;
+  }
+}
+Player.prototype.hold = function() {
+  this.totalScore += this.turnScore;
+  this.turnScore = 0;
+  alert("Pass the mouse");
+}
+Player.prototype.winnerCheck = function() {
+  if (this.totalScore >= 100) {
+    alert(" You won");
+  }
+}
+var player1 = new Player();
+var player2 =  new Player();
+
+$(document).ready(function() {
+  $("button#rol1").click(function(){
+    player1.roll = rollDice();
+    $("#score1").text(player1.roll);
+    player1.roll1();
+    $("#points1").text(player1.turnScore);
+  });
+  $("button#roll2").click(function(){
+    player2.roll = rollDice();
+    $("#value2").text(player2.roll);
+    player2.roll1();
+    $("#points2").text(player2.turnScore);
+  });
+
+  $("button#hold1").click(function(){
+  player1.hold();
+  $("#score1").text(player1.totalScore);
+  $("#points1").empty();
+  $("#value1").empty();
+  player1.winnerCheck();
+  });
+  $("button#hold2").click(function(){
+  player2.hold();
+  $("#score2").text(player2.totalScore);
+  $("#points2").empty();
+  $("#value2").empty();
+  player2.winnerCheck();
+  });
+})
