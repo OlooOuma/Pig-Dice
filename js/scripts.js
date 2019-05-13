9d2859abac73bbf6ee08c98c16a579d384e33d7e
Oloo Ouma <oloosolomon@gmail.com>
2019-05-13 07:57:47 +0300

    Add Js function

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,9 @@ function Player(turn) {
   this.turnScore = 0;
   this.totalScore = 0;
 }
-function rollDice(){
-  return Math.floor(6 * Math.random())+1;
-}
-Player.prototype.startGame = function() {
-  this.roll = 0;
-  this.turnScore = 0;
-  this.totalScore = 0;
-  this.playerName = "";
-}
 Player.prototype.roll1 = function() {
   if (this.roll === 1) {
-    this.turnScore = 0;
+    this.turnScore = 6;
     alert("Next Player");
   } else {
     this.turnScore += this.roll;
@@ -48,18 +39,16 @@ $(document).ready(function() {
     $("#points2").text(player2.turnScore);
   });
 
-  // $("#hold1").click(function(){
-  // player1.hold();
-  // $("#score1").text(player1.totalScore);
-  // $("#points1").empty();
-  // $("#poi1").empty();
-  // player1.winnerCheck();
-  // });
-  // $("#hold2").click(function(){
-  // player2.hold();
-  // $("#score2").text(player2.totalScore);
-  // $("#points2").empty();
-  // $("#poi2").empty();
-  // player2.winnerCheck();
-  // });
-});
+  $("#hold1").click(function(){
+  player1.hold();
+  $("#score1").text(player1.totalScore);
+  $("#points1").empty();
+  $("#poi1").empty();
+  });
+  $("#hold2").click(function(){
+  player2.hold();
+  $("#score2").text(player2.totalScore);
+  $("#points2").empty();
+  $("#poi2").empty();
+  });
+})
