e1bc52a52f3d5165ef9715b0c9f4e9a2942a3845
Oloo Ouma <oloosolomon@gmail.com>
2019-05-13 12:03:44 +0300

    Add a click function on Roll

--- js/scripts.js
+++ js/scripts.js
@@ -7,52 +7,17 @@ $(document).ready(function(){
       $(".player2").slideToggle();
     });
 });
-// function Player(turn) {
-//   this.turn = turn;
-//   this.roll = 0;
-//   this.turnScore = 0;
-//   this.totalScore = 0;
-// }
-// Player.prototype.roll1 = function() {
-//   if (this.roll === 1) {
-//     this.turnScore = 6;
-//     alert("Next Player");
-//   } else {
-//     this.turnScore += this.roll;
-//   }
-// }
-// Player.prototype.hold = function() {
-//   this.totalScore += this.turnScore;
-//   this.turnScore = 0;
-//   alert("Next Player");
-// }
-// var player1 = new Player();
-// var player2 =  new Player();
-//
-// $(document).ready(function() {
-//   $("#rol1").click(function(){
-//     player1.roll = rollDice();
-//     $("#score1").text(player1.roll);
-//     player1.roll1();
-//     $("#points1").text(player1.turnScore);
-//   });
-//   $("#roll2").click(function(){
-//     player2.roll = rollDice();
-//     $("#score2").text(player2.roll);
-//     player2.roll1();
-//     $("#points2").text(player2.turnScore);
-//   });
-//
-//   $("#hold1").click(function(){
-//   player1.hold();
-//   $("#score1").text(player1.totalScore);
-//   $("#points1").empty();
-//   $("#poi1").empty();
-//   });
-//   $("#hold2").click(function(){
-//   player2.hold();
-//   $("#score2").text(player2.totalScore);
-//   $("#points2").empty();
-//   $("#poi2").empty();
-//   });
-// })
+var diceRoll,roundScore1,finalScore1,finalScore2;
+roundScore1 = 0;
+roundScore2 = 0;
+finalScore1 = 0;
+finalscore2 = 0;
+
+$("#rol1").click(function(){
+  var randomNo = Math.floor((Math.random() * 6) + 1);
+  if (randomNo === 1){
+    roundScore1 = 0;
+    $("#diceRoll1").text(randomNo);
+    $("#roundScore1").text(roundScore1);
+  }
+})
